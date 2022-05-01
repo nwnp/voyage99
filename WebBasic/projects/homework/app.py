15d564752de39f84ad64c9d77a01b11c22b69beb
nwnp <jjw_0418@naver.com>
2022-05-01 21:40:54 +0900

    feat: GET, POST 방식 구현 완료

--- WebBasic/projects/homework/app.py
+++ WebBasic/projects/homework/app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.wze36.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,13 +13,24 @@ def home():
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
-  sample_receive = request.form['sample_give']
-  print(sample_receive)
+  name_receive = request.form['name_give']
+  comment_receive = request.form['comment_give']
+
+  print('server', name_receive, comment_receive)
+  
+  doc = {
+    'name': name_receive,
+    'comment': comment_receive,
+  }
+  
+  db.fans.insert_one(doc)
+
   return jsonify({'msg':'POST 연결 완료!'})
 
 @app.route("/homework", methods=["GET"])
 def homework_get():
-  return jsonify({'msg':'GET 연결 완료!'})
+  all_comments = list(db.fans.find({},{'_id':False}))
+  return jsonify({'msg':all_comments})
 
 if __name__ == '__main__':
   app.run('0.0.0.0', port=5001, debug=True)
